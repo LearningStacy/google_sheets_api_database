1fc45cdbaf725c6161d89861c7bb89529732f5aa
LearningStacy <stacy.saintpaul@gmail.com>
2024-03-16 05:42:59 -0400

    implement read from spreadsheet method

--- lib/models/google_sheets_api.dart
+++ lib/models/google_sheets_api.dart
@@ -1,4 +1,5 @@
 import 'package:gsheets/gsheets.dart';
+import 'package:search_database_sheets_api/models/PokemonFields.dart';
 
 class GoogleSheetsAPI {
   static const _credentials = r'''
@@ -13,6 +14,13 @@ class GoogleSheetsAPI {
   static Future init() async{
     final spreadsheet = await _gsheets.spreadsheet(_spreadsheetID);
     _pokemonSheet = spreadsheet.worksheetByTitle('Pokemon Table');
+  }
+
+  static Future<Pokemon?> getById(int id) async{
+    if (_pokemonSheet == null) return null;
+
+    final json = await _pokemonSheet!.values.map.rowByKey(id, fromColumn: 1);
+      return json ==null ? null:Pokemon.fromJson(json);
   }
 
 
