411e3c63ba887ba26e4bb347b69fed680b7ce5c0
LearningStacy <stacy.saintpaul@gmail.com>
2024-03-16 05:00:24 -0400

    create model for table fields

---
+++ lib/models/PokemonFields.dart
@@ -0,0 +1,8 @@
+class PokemonFields{
+  static final String id='ID';
+  static final String name = 'Name of Pokemon';
+  static final String elementType = 'Element Type';
+  static final String generation = 'Generation';
+
+
+}
